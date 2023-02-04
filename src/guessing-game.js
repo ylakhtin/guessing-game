8b6d05b2783463dc84355cfd6bd8d800104b61c5
Yuriy Lakhtin <yuriy.lakhtin@gmail.com>
2023-02-04 21:36:50 +0100

    feat: add the guesser

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.mid = this.max - Math.floor((this.max - this.min)/2);
     }
 
     guess() {
-
+        // console.log('Max:', this.max, ' Min: ', this.min, ' RETRURN GUESS CANDIDATE - ', this.mid);
+        return this.mid;
     }
 
     lower() {
-
+        this.setRange(this.min, this.mid);
     }
 
     greater() {
-
+        this.setRange(this.mid, this.max);
     }
 }
 
